3d246496b248b016d0596f061170b4114565c970
Duy Truong <truongbaoduy2003@gmail.com>
2025-03-20 22:21:06 +0700

    Added templates gallery

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
+
 import { Navbar } from "./navbar";
+import { TemplatesGallery } from "./templates-gallery";
 
 const Home = () => {
   return (
@@ -8,7 +10,7 @@ const Home = () => {
         <Navbar />
       </div>
       <div className="mt-16">
-        <Link href="/documents/123">Click me</Link>
+        <TemplatesGallery />
       </div>
     </div>
   );
