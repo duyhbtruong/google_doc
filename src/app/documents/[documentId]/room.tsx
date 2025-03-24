8d2a404ff0a572facaf539e269d62ac9fd641d36
Duy Truong <truongbaoduy2003@gmail.com>
2025-03-24 10:46:00 +0700

    Setup collaboration feature using Liveblocks

---
+++ src/app/documents/[documentId]/room.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { ReactNode } from "react";
+import {
+  LiveblocksProvider,
+  RoomProvider,
+  ClientSideSuspense,
+} from "@liveblocks/react/suspense";
+import { useParams } from "next/navigation";
+
+export function Room({ children }: { children: ReactNode }) {
+  const params = useParams();
+
+  return (
+    <LiveblocksProvider
+      publicApiKey={
+        "pk_dev_WQGn8Z4J-_vXZpE47jBHhNzEKP1ToPksrLIqT4JIKTxgneHUIBo_PuGCRzGV9_DW"
+      }
+    >
+      <RoomProvider id={params.documentId as string}>
+        <ClientSideSuspense fallback={<div>Loading…</div>}>
+          {children}
+        </ClientSideSuspense>
+      </RoomProvider>
+    </LiveblocksProvider>
+  );
+}
