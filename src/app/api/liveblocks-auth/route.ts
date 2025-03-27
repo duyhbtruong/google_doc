ab4d59d1a1aec9954f5184aa875575f77e8a168d
Duy Truong <truongbaoduy2003@gmail.com>
2025-03-27 16:39:55 +0700

    Added notification feature

--- src/app/api/liveblocks-auth/route.ts
+++ src/app/api/liveblocks-auth/route.ts
@@ -5,6 +5,7 @@ import { auth, currentUser } from "@clerk/nextjs/server";
 import { api } from "../../../../convex/_generated/api";
 
 const convex = new ConvexHttpClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+
 const liveblocks = new Liveblocks({
   secret: process.env.LIVEBLOCKS_SECRET_KEY!,
 });
--- src/app/documents/[documentId]/navbar.tsx
+++ src/app/documents/[documentId]/navbar.tsx
@@ -41,6 +41,7 @@ import {
 
 import { useEditorStore } from "@/store/use-editor-store";
 import { Avatars } from "./avatars";
+import { Inbox } from "./inbox";
 
 export const Navbar = () => {
   const { editor } = useEditorStore();
@@ -266,6 +267,7 @@ export const Navbar = () => {
       </div>
       <div className="flex gap-3 items-center pl-6">
         <Avatars />
+        <Inbox />
         <OrganizationSwitcher
           afterCreateOrganizationUrl="/"
           afterLeaveOrganizationUrl="/"
